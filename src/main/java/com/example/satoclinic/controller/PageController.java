773f6807fc2dabb752edec744565bbc870f39032
DESKTOP-NUOURCF\academia <202512_aw_ishikawa@x-ide.sakura.ne.jp>
2026-05-29 16:21:46 +0900

    フロントをJavaプロジェクトに変換

---
+++ src/main/java/com/example/satoclinic/controller/PageController.java
@@ -0,0 +1,28 @@
+﻿package com.example.satoclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping({"/", "/index.html"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping({"/faq", "/faq.html"})
+    public String faq() {
+        return "faq";
+    }
+
+    @GetMapping({"/medical-info", "/medical-info.html"})
+    public String medicalInfo() {
+        return "medical-info";
+    }
+
+    @GetMapping({"/news", "/news.html"})
+    public String news() {
+        return "news";
+    }
+}
